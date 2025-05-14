6a31391a59cec43c865c9347fc754e909fb67ca7
Bermx97 <bermxtf@gmail.com>
2025-05-14 20:20:35 +0200

    creating a server

---
+++ main.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const PORT = 3001
+
+app.use(express.static(__dirname + "/public"));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+})
+
+
+
+
+    
+
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`)
+})
