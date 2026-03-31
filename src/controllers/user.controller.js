5f6247f5f7c4a7ff4e720b82e4c25c2500af1db5
Bermx97 <bermxtf@gmail.com>
2026-03-31 19:29:52 +0200

    feat: allow viewing other users profile

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,16 +5,18 @@ const userService = require('../services/user.service');
 
 exports.showUser = async (req, res) => {
     const wantedUser = req.params.id;
-    const userName = req.session.userName;
-    if (wantedUser !== userName) {
-        return res.status(403).send('You are not allowed to view this profile');
-    };
-    const findUser = await loginService.findUser(userName);
+    const findUser = await loginService.findUser(wantedUser);
+    const loggedUser = req.session.userName
+    if (!findUser.rows[0]) {
+        const error = new Error('User not found');
+        error.status = 404;
+        throw error;
+    }
     const user = findUser.rows[0];
     const totalLikesResult = await likeService.countLikesForUser(user.id);
     const totalLikes = (totalLikesResult.rows[0].total_likes);
     const totalCaptionsResult = await captionService.countCaptionsForUser(user.id);
     const totalCaptions = (totalCaptionsResult.rows[0].count);
     const captions = await userService.getUserCaptions(user.id);
-    res.render('user', { username: user.username, role: user.role, totalLikes, totalCaptions, captions });
-    };
+    res.render('user', { username: user.username, role: user.role, totalLikes, totalCaptions, captions, loggedUser: loggedUser });
+};
